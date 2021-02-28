abe0fc50477ccf084ab1e820260523f8ad768499
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-02-28 17:07:08 +0530

    Create deployment workflow (#12)
    * Update editor config
    
    * Create deploy.yml
    
    * Use access token
    
    * Update workflow for master

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,9 @@
 module.exports = {
 	arrowParens: 'always',
 	bracketSpacing: true,
-	endOfLine: 'lf',
 	jsxSingleQuote: true,
 	printWidth: 120,
 	semi: true,
 	singleQuote: true,
-	tabWidth: 4,
 	trailingComma: 'es5',
-	useTabs: true,
 };
